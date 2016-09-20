b5223a362f506943e63c728667fcf6d99a07e37f
sbobrov85 <sbobrov85@gmail.com>
2016-09-20 20:28:42 +0500

    #1 Initial commit

---
+++ ModuleHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+*  @author    sbobrov85<sbobrov85@gmail.com>
+*  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class ModuleHelper
+{
+    
+}
