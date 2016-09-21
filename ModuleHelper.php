bdac9a251fe8e269b745082fa28a03050a8ef6b9
sbobrov85 <sbobrov85@gmail.com>
2016-09-21 07:24:53 +0500

    #2 Migrate methods from ps-sociallikes

--- ModuleHelper.php
+++ ModuleHelper.php
@@ -11,5 +11,76 @@ if (!defined('_PS_VERSION_')) {
 
 class ModuleHelper
 {
-    
+    private static $SETTINGS_PREFIX = '';
+
+    /**
+     * Set class property for settings prefix
+     * @param string $settingsPrefix settings prefix
+     * @throws Exception if settingsPrefix is empty
+     */
+    public static function setSettingsPrefix($settingsPrefix)
+    {
+        if (empty($settingsPrefix)) {
+            throw new Exception('Settings prefix require non empty value!');
+        }
+
+        $settingsPrefix = rtrim($settingsPrefix, '_') . '_';
+        self::$SETTINGS_PREFIX = Tools::strtoupper($settingsPrefix);
+    }
+
+    /**
+     * Build module settings key
+     * @param string $paramName param name
+     * @return string full settings key
+     * @throws Exception if settings prefix not set
+     */
+    public static function buildSettingsKey($paramName)
+    {
+        if (empty(self::$SETTINGS_PREFIX)) {
+            throw new Exception('Requre settings prefix for using method!');
+        }
+
+        return self::$SETTINGS_PREFIX . Tools::strtoupper($paramName);
+    }
+
+    /**
+     * Get config field value by param name
+     * @param string $paramName param name
+     * @return mixed config value
+     * @throws Exception if param name is empty
+     */
+    public static function getConfigFieldValue($paramName)
+    {
+        if (empty($paramName)) {
+            throw new Exception('Require not empty param name!');
+        }
+
+        $settingsKey = self::buildSettingsKey($paramName);
+
+        return Tools::getValue(
+            $settingsKey,
+            Configuration::get($settingsKey)
+        );
+    }
+
+    /**
+     * Set config field value by param name
+     * @param string $paramName param name
+     * @param mixed $value value for set, if null then read from request
+     * @throws Exception if param name is empty
+     */
+    public static function setConfigFieldValue($paramName, $value = null)
+    {
+        if (empty($paramName)) {
+            throw new Exception('Require not empty param name!');
+        }
+
+        $settingsKey = self::buildSettingsKey($paramName);
+
+        if (!isset($value)) {
+            $value = Tools::getValue($settingsKey);
+        }
+
+        Configuration::updateValue($settingsKey, $value);
+    }
 }
